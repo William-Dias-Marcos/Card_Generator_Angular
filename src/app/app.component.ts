34a7ed7b4210797ca9010a956fffe9d4757e061f
William-Dias-Marcos <william.diasmarcos@gmail.com>
2023-10-03 17:16:39 -0300

    initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+import { FormBuilder, Validators } from '@angular/forms';
+
+import html2canvas from 'html2canvas';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cardGenerator';
+
+  constructor(private formBuilder : FormBuilder) { }
+
+  profileForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    profession: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    phoneNumber: ['', [Validators.required, Validators.minLength(8)]],
+    address: this.formBuilder.group({
+      city: ['', Validators.required], 
+      state: ['', Validators.required]
+    })
+  });
+
+  submitForm(){
+    console.log(this.profileForm.value)
+  }
+
+      // html2canvas().then(canvas => {
+      //   const downloadLink = document.createElement('a') as HTMLAnchorElement;
+      //   downloadLink.href = canvas.toDataURL('image/png');
+      //   downloadLink.download = 'minha-imagem.png';
+      //   downloadLink.click();
+      // });
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
